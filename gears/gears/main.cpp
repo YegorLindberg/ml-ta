ecacab853d1b445fcaaf2468070b811a4544c198
YegorLindberg <mr.tsarap@mail.ru>
2018-03-11 18:50:25 +0300

    2.10

--- gears/gears/main.cpp
+++ gears/gears/main.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 const int FILE_IS_NOT_OPENED = 1;
 const int NECESSARY_AMOUNT_OF_ARGUMENTS = 2;
+const int WRONG_AMOUNT_OF_ARGUMENTS = 3;
 
 struct pairOfGears
 {
@@ -30,7 +31,7 @@ int main(int argc, const char * argv[]) {
     {
         std::cout << " You should enter 2 arguments:\n" <<
         "\treplace.exe <input file> <output file>\n";
-        return 1;
+        return WRONG_AMOUNT_OF_ARGUMENTS;
     }
     
     const string inputFileName = argv[1];
@@ -50,117 +51,112 @@ int main(int argc, const char * argv[]) {
     }
     
     //          main code:
-
     
-    vector<string> result; // запись результата пары шестеренок.
-    vector<pairOfGears> mainPairs;
-    bool firstEnter = true;
+    //initializaiton
     int amountOfGears = 0;
     int amountOfPairs = 0;
-    while (!inputFile.eof())
+    inputFile >> amountOfGears >> amountOfPairs;
+    vector<string> result(amountOfGears); // запись результата для каждой шестеренки(номер шестеренки является (порядок - 1))
+    vector<pairOfGears> mainPairs(amountOfPairs);
+    
+    for (int i = 0; i < amountOfPairs; ++i)
     {
-        if(firstEnter == false)
-        {
-            int firstNumber = 0;
-            int secondNumber = 0;
-            inputFile >> firstNumber >> secondNumber;
-            pairOfGears temp;
-            temp.firstNum = firstNumber;
-            temp.secondNum = secondNumber;
-            temp.movingFirst = temp.movingSecond = "undefined";
-            mainPairs.push_back(temp);
-        }
-        else
-        {
-            inputFile >> amountOfGears;
-            inputFile >> amountOfPairs;
-            firstEnter = false;
-        }
+        int firstNumber = 0;
+        int secondNumber = 0;
+        inputFile >> firstNumber >> secondNumber;
+        pairOfGears temp;
+        temp.firstNum = firstNumber;
+        temp.secondNum = secondNumber;
+        temp.movingFirst = temp.movingSecond = "undefined";
+        mainPairs[i] = temp;
     }
     
-    while(amountOfGears + 1 != 0)
-    {
-        result.push_back("immovable");
-        --amountOfGears;
-    }
+    for (int j = 0; j < amountOfGears; ++j)
+        result[j] = "immovable";
     
-        //initialization
-    mainPairs.erase(mainPairs.end() - 1);
-    result.erase(result.end() - 1);
-    result[0] = "BEGIN:";
     mainPairs[0].movingFirst = "clockwise";
     mainPairs[0].movingSecond = "unclockwise";
-    result[mainPairs[0].firstNum] = "clockwise";
-    result[mainPairs[0].secondNum] = "unclockwise";
+    result[mainPairs[0].firstNum - 1] = "clockwise";
+    result[mainPairs[0].secondNum - 1] = "unclockwise";
     mainPairs.erase(mainPairs.begin());
-    --amountOfPairs;
     
+    //magic
     bool breaker = false;
     bool change = true;
     
-    while(change == true)
+    while (change == true)
     {
         change = false;
-        for (int i = 0; i < amountOfPairs; ++i)
+        for (int i = 0; i < mainPairs.size(); ++i)
         {
-        
-            //сделай для перечислений норм крч, не через циклы. Хорошая идея была же
-        
-            //cout << amountOfPairs << " - amount of pairs\n";
-            if (((mainPairs[i].firstNum == j) && (mainPairs[i].movingFirst != result[j])) ||
-                ((mainPairs[i].secondNum == j) && (mainPairs[i].movingSecond != result[j])))
+            if ((result[mainPairs[i].firstNum - 1] != "immovable") && (mainPairs[i].movingFirst == "undefined"))
             {
+                mainPairs[i].movingFirst = result[mainPairs[i].firstNum - 1];
                 change = true;
-                if (mainPairs[i].firstNum == j)
-                    mainPairs[i].movingFirst = result[j];
-                else
-                    mainPairs[i].movingSecond = result[j];
-                if (((mainPairs[i].movingFirst == "clockwise") && (mainPairs[i].movingSecond == "unclockwise")) ||
-                    ((mainPairs[i].movingFirst == "unclockwise") && (mainPairs[i].movingSecond == "clockwise")))
-                {
-                    mainPairs.erase(mainPairs.begin() + i);
-                    --amountOfPairs;
-                }
-                else if (((mainPairs[i].movingFirst == "unclockwise") && (mainPairs[i].movingSecond == "unclockwise")) ||
-                         ((mainPairs[i].movingFirst == "clockwise") && (mainPairs[i].movingSecond == "clockwise")))
-                {
-                    change = false;
-                    breaker = true;
-                    break;
-                }
             }
-
+            else if ((mainPairs[i].movingSecond != "undefined") && (mainPairs[i].movingFirst == "undefined"))
+            {
+                if (mainPairs[i].movingSecond == "clockwise")
+                    mainPairs[i].movingFirst = "unclockwise";
+                else
+                    mainPairs[i].movingFirst = "clockwise";
+                result[mainPairs[i].firstNum - 1] = mainPairs[i].movingFirst;
+                change = true;
+            }
+            if ((result[mainPairs[i].secondNum - 1] != "immovable") && (mainPairs[i].movingSecond == "undefined"))
+            {
+                mainPairs[i].movingSecond = result[mainPairs[i].secondNum - 1];
+                change = true;
+            }
+            else if ((mainPairs[i].movingFirst != "undefined") && (mainPairs[i].movingSecond == "undefined"))
+            {
+                if (mainPairs[i].movingFirst == "clockwise")
+                    mainPairs[i].movingSecond = "unclockwise";
+                else
+                    mainPairs[i].movingSecond = "clockwise";
+                result[mainPairs[i].secondNum - 1] = mainPairs[i].movingSecond;
+                change = true;
+            }
+            /* //пробовал с удалением, думал, что будет быстрее, но оказалось наоборот.
+            if ((mainPairs[i].movingFirst == "clockwise" && mainPairs[i].movingSecond == "unclockwise") ||
+                (mainPairs[i].movingFirst == "unclockwise" && mainPairs[i].movingSecond == "clockwise"))
+                mainPairs.erase(mainPairs.begin() + i);
+             */
+            if ((mainPairs[i].movingFirst == "clockwise" && mainPairs[i].movingSecond == "clockwise") ||
+                (mainPairs[i].movingFirst == "unclockwise" && mainPairs[i].movingSecond == "unclockwise"))
+            {
+                breaker = true;
+                change = false;
+                break;
+            }
         }
-        
     }
     
     //conclusion
     if(breaker == false)
     {
+        /* //только для проверки некоторых случаев:
         for (int i = 0; i < mainPairs.size(); ++i)
         {
             cout << mainPairs[i].firstNum << ":" << mainPairs[i].secondNum << " - " << mainPairs[i].movingFirst << ", " << mainPairs[i].movingSecond <<"\n";
         }
+        */
         for (int i = 0; i < result.size(); ++i)
         {
-            cout << result[i] <<"\n";
+            outputFile << result[i] <<"\n";
         }
     }
     else
-        cout << "blocked\n";
-    
-    
-    
-    
-    // end of main code.
+        outputFile << "blocked\n";
     
     if (!outputFile.flush())
     {
         cout << "Failed to save data on disk\n";
         return 3;
     }
+    
     inputFile.close();
     outputFile.close();
-    //cout << "work is done.\n";
+
     return 0;
 }
